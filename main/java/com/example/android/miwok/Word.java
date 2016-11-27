04dee10134fb5493119529e245b8e28c9d4666b5
Avi Schwartz <Schwartz210@gmail.com>
2016-11-27 12:14:30 -0500

    first add
    first add

---
+++ main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,39 @@
+/**
+ * Created by Avi on 11/26/2016.
+ */
+package com.example.android.miwok;
+
+
+public class Word {
+    private String mDefaultTranslation;
+    private String mMiwokTranslation;
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+    public Word(String defaultTranslation, String miwokTranslation){
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    public Word(String defaultTranslation, String miwokTranslation, int resourceId){
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = resourceId;
+    }
+
+    public String getDefaultTranslation(){
+        return mDefaultTranslation;
+    }
+
+    public String getMiwokTranslation(){
+        return mMiwokTranslation;
+    }
+
+    public int getImageResourceId(){
+        return mImageResourceId;
+    }
+
+    public boolean hasImage(){
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+}
---
+++ main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,50 @@
+package com.example.android.miwok;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Avi on 11/26/2016.
+ */
+
+public class WordAdapter extends ArrayAdapter<Word> {
+    public WordAdapter(Activity context, ArrayList<Word> words) {
+        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+        // the second argument is used when the ArrayAdapter is populating a single TextView.
+        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
+        // going to use this second argument, so it can be any value. Here, we used 0.
+        super(context, 0, words);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        Word currentWord = getItem(position);
+        TextView nameTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        nameTextView.setText(currentWord.getDefaultTranslation());
+        TextView numberTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        numberTextView.setText(currentWord.getMiwokTranslation());
+        ImageView iconView = (ImageView) listItemView.findViewById(R.id.image);
+        if (currentWord.hasImage()){
+            iconView.setImageResource(currentWord.getImageResourceId());
+            iconView.setVisibility(View.VISIBLE);
+        } else {
+            iconView.setVisibility(View.GONE);
+        }
+        return listItemView;
+    }
+}
